8549d77b65e368a00df144cd6df9738b0c557c47
li_zhiqi <8140671+li-zhiqi@user.noreply.gitee.com>
2021-03-27 15:43:34 +0800

    3 15:43

--- cal.h
+++ cal.h
@@ -12,13 +12,21 @@ Vector4d Newton(Vector4d u0, const double mu, const double k, Vector4d b);
 
 Vector4d initial_load(const char _file[],double* _mu);
 
-Vector4d AB_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info& info);
-Vector4d AB_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info_Table& table);
-Vector4d AB_method(Vector4d u0, const double dt, double mu, int _acc, int N, const Info_Table& table);
+Vector4d AB_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info& info);
+Vector4d AB_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info_Table& table);
+Vector4d AB_method(Vector4d u0, const double dt, const double mu, int _acc, int N, const Info_Table& table);
 
-Vector4d AM_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info& info);
-Vector4d AM_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info_Table& table);
-Vector4d AM_method(Vector4d u0, const double dt, double mu, int _acc, int N, const Info_Table& table);
+Vector4d AM_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info& info);
+Vector4d AM_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info_Table& table);
+Vector4d AM_method(Vector4d u0, const double dt, const double mu, int _acc, int N, const Info_Table& table);
+
+Vector4d BDF_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info& info);
+Vector4d BDF_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info_Table& table);
+Vector4d BDF_method(Vector4d u0, const double dt, const double mu, int _acc, int N, const Info_Table& table);
+
+Vector4d RK_one_step(Vector4d u, const double dt, const double mu);
+Vector4d RK_method(Vector4d u0, const double dt, const double mu, int N);
+  
 
 Vector4d f(Vector4d u, const double mu){
   double v0,v1,v2,v3;
@@ -62,14 +70,15 @@ Vector4d Newton(Vector4d u0, const double mu, const double k, Vector4d b){
   Matrix4d M = DF(v,mu,k);
   Vector4d r = -F(v,mu,k,b);
   Vector4d dv;
-  dv = M.colPivHouseholderQr().solve(r);
+  dv = M.fullPivHouseholderQr().solve(r);
   v += dv;
   r = -F(v,mu,k,b);
   while( r.squaredNorm() > 10e-16){
     M = DF(v,mu,k);
-    dv = M.colPivHouseholderQr().solve(r);
+    dv = M.fullPivHouseholderQr().solve(r);
     v += dv;
     r = -F(v,mu,k,b);
+    std::cout << r.squaredNorm() << std::endl;
   }
   return v;
 }
@@ -85,19 +94,19 @@ Vector4d initial_load(const char _file[],double* _mu){
   return u;
 }
 
-Vector4d AB_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info& info){
+Vector4d AB_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info& info){
   Vector4d v=u[_acc-1];
   for (int i = 1; i <= _acc ; i++)
     v += f(u[_acc-i],mu)*info.get_coe(i-1)*dt;
   return v;
 }
 
-Vector4d AB_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info_Table& table){
+Vector4d AB_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info_Table& table){
   const Info& AB_info=table.get_info(_acc);
   return AB_one_step(u,dt,mu,_acc,AB_info);
 }
 
-Vector4d AB_method(Vector4d u0, const double dt, double mu, int _acc, int N, const Info_Table& table){
+Vector4d AB_method(Vector4d u0, const double dt, const double mu, int _acc, int N, const Info_Table& table){
   Vector4d* u;
   u = new Vector4d [_acc];
   u[0] = u0;
@@ -130,7 +139,7 @@ Vector4d AB_method(Vector4d u0, const double dt, double mu, int _acc, int N, con
   return u[_acc-1];
 }
 
-Vector4d AM_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info& info){
+Vector4d AM_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info& info){
   Vector4d b = u[_acc-2];
   for ( int i = 2; i <= _acc; i++)
     b += f(u[_acc-i],mu)*info.get_coe(i-1)*dt;
@@ -138,12 +147,12 @@ Vector4d AM_one_step(Vector4d* u, const double dt, double mu, int _acc, const In
   return v;
 }
 
-Vector4d AM_one_step(Vector4d* u, const double dt, double mu, int _acc, const Info_Table& table){
+Vector4d AM_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info_Table& table){
   const Info& AM_info=table.get_info(_acc);
   return AM_one_step(u,dt,mu,_acc,AM_info);
 }
 
-Vector4d AM_method(Vector4d u0, const double dt, double mu, int _acc, int N, const Info_Table& table){
+Vector4d AM_method(Vector4d u0, const double dt, const double mu, int _acc, int N, const Info_Table& table){
   Vector4d* u;
   u = new Vector4d [_acc-1];
   u[0] = u0;
@@ -174,4 +183,81 @@ Vector4d AM_method(Vector4d u0, const double dt, double mu, int _acc, int N, con
   os << "plot(x,y)";
   os.close();
   return u[_acc-2];
+}
+
+
+Vector4d BDF_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info& info){
+  Vector4d b;
+  b << 0,0,0,0;
+  for ( int i = 1; i <= _acc; i++)
+    b += u[_acc-i]*info.get_coe(i-1);
+  Vector4d v = Newton(u[_acc-1],mu,-dt*info.get_coe(_acc),-b);
+  return v;
+}
+
+Vector4d BDF_one_step(Vector4d* u, const double dt, const double mu, int _acc, const Info_Table& table){
+  const Info& BDF_info=table.get_info(_acc);
+  return BDF_one_step(u,dt,mu,_acc,BDF_info);
+}
+
+Vector4d BDF_method(Vector4d u0, const double dt, const double mu, int _acc, int N, const Info_Table& table){
+  Vector4d* u;
+  u = new Vector4d [_acc];
+  u[0] = u0;
+  int k = 1;
+  while (k < _acc){
+    Vector4d v = BDF_one_step(u,dt,mu,k,table);
+    u[k] = v;
+    k++;
+  }
+  std::ofstream os;
+  std::string ss = "BDF_" + std::to_string(_acc) + "_" + std::to_string(N) + ".m";
+  const char *s = ss.c_str();
+  os.open(s);
+  os << "a=[\n";
+  for (int i = 0; i < _acc ; i++)
+    os << u[i](0) << "," << u[i](1) << ";\n";
+  const Info& BDF_info = table.get_info(_acc);
+  for (int i = 0; i < N - _acc + 1; i++){
+    Vector4d v = BDF_one_step(u,dt,mu,_acc,BDF_info);
+    for (int j = 0; j < _acc - 1 ; j++)
+      u[j] = u[j+1];
+    u[_acc-1] = v;
+    os << v(0) << "," << v(1) << ";\n";
+  }
+  os << "];\n";
+  os << "x = a(:,1);";
+  os << "y = a(:,2);";
+  os << "plot(x,y)";
+  os.close();
+  return u[_acc-1];
+}
+
+Vector4d RK_one_step(Vector4d u, const double dt, const double mu){
+  Vector4d k1 = dt * f(u,mu);
+  Vector4d k2 = dt * f(u + 0.5 * k1,mu);
+  Vector4d k3 = dt * f(u + 0.5 * k2,mu);
+  Vector4d k4 = dt * f(u + k3,mu);
+  return u + (k1 + 2 * k2 + 2 * k3 + k4)/6;
+}
+
+
+Vector4d RK_method(Vector4d u0, const double dt, const double mu, int N){
+  Vector4d u = u0;
+  std::ofstream os;
+  std::string ss = "RK_" + std::to_string(N) + ".m";
+  const char *s = ss.c_str();
+  os.open(s);
+  os << "a=[\n";
+  for (int i = 0; i < N ; i++){
+    Vector4d v = RK_one_step(u,dt,mu);
+    u = v;
+    os << u(0) << "," << u(1) << ";\n";
+  }
+  os << "];\n";
+  os << "x = a(:,1);";
+  os << "y = a(:,2);";
+  os << "plot(x,y)";
+  os.close();
+  return u;
 }
--- test.cpp
+++ test.cpp
@@ -18,10 +18,10 @@ int main( int argc, char* agrv[]){
   const Info& AM_info = AM.get_info(7);
   for (int i = 0; i < 5 ; i++)
     std::cout << AM_info.get_coe(i) << ",";
-  std::cout << "\b " << std::endl;
+  std::cout << "\b " << std::endl;*/
   Info_Table BDF;
   BDF.load_data("BDFs",3);
-  std::cout << BDF.get_n_table() << " " << BDF.get_type() << std::endl;
+  /*std::cout << BDF.get_n_table() << " " << BDF.get_type() << std::endl;
   const Info& BDF_info = BDF.get_info(3);
   for (int i = 0; i < 4 ; i++)
     std::cout << BDF_info.get_coe(i) << ",";
@@ -29,7 +29,7 @@ int main( int argc, char* agrv[]){
   Vector4d u0;
   double mu;
   u0 = initial_load("Initial1",&mu);
-  double dt = 0.0001;
+  double dt = 0.001;
   /*Vector4d v = AB_one_step(u0,dt,mu,1,AB);
   std::cout << v << std::endl;
   u[0] = v;
@@ -41,12 +41,13 @@ int main( int argc, char* agrv[]){
   u[0] = v;
   v = AB_one_step(u,dt,mu,1,AB);
   std::cout << v << std::endl;*/
-  Vector4d v = AB_method(u0,dt,mu,4,180000,AB);
-  std::cout << v << std::endl;
+  //Vector4d v = AB_method(u0,dt,mu,4,1000000,AB);
+  //std::cout << v << std::endl;
   //Vector4d w = AM_method(u0,dt,mu,3,90000,AM);
   //std::cout << w << std::endl;
-  //Vector4d z = Newton(u0,mu,-0.5*dt,u0+0.5*dt*f(u0,mu));
-  //std::cout << z << std::endl;
-  
+  Vector4d z = BDF_method(u0,dt,mu,4,17000,BDF);
+  std::cout << z << std::endl;
+  //Vector4d r = RK_method(u0,dt,mu,20000);
+  //std::cout << r << std::endl;
   return 0;
 }
