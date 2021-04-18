63d05dbdb5100fef68e217252c386e841cee8061
li_zhiqi <8140671+li-zhiqi@user.noreply.gitee.com>
2021-04-18 11:33:08 +0800

    12 11:33

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
-all: test2.o Info.o 
-	g++ -o test2 test2.o Info.o -std=c++11
+all: main.o Info.o 
+	g++ -o main main.o Info.o -std=c++11
 Info.o: Info.h Info.cpp
 	g++ -c Info.cpp 
-test2.o: test2.cpp 
-	g++ -c test2.cpp  -std=c++11
+main.o: main.cpp 
+	g++ -c main.cpp  -std=c++11
 Factory.o: Factory.h Factory.cpp
 	g++ -c Factory.cpp -std=c++11
 clean:
 	rm *.o
-	rm test2
+	rm main
 cleanm:
 	rm *.o
 	rm *.m
-	rm test2
+	rm main
--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 #include "Factory.cpp"
+
+void getInput(std::fstream &_Input, std::string &_Method, int &_Order, double &_dt, std::string &_Initial, int &_N, bool &_err_analysis, bool &_grid_refine_analysis){
+  _Input >> _Method >> _Order >> _dt
+	 >> _Initial  >> _N
+	 >> _err_analysis >> _grid_refine_analysis;
+}
+
+
+int main(int argc, char* argv[]){
+  std::string garbage,Method,Initial;
+  double dt,T;
+  Vector4d u0;
+  int Order,N;
+  bool err_analysis,grid_refine_analysis;
+  std::fstream Input("Input");
+  getline(Input,garbage);
+  while (getline(Input,garbage,'\n')){
+    getInput(Input,Method,Order,dt,Initial,N,err_analysis,grid_refine_analysis);
+  }
+  std::cout << Method << std::endl << Order << std::endl << dt <<std::endl << Initial <<std::endl << N <<std::endl << err_analysis <<std::endl << grid_refine_analysis << std::endl;
+
+  return 0;
+}
