b4803b421a7b712c9d7d92736c6bbc16eaab84d8
li_zhiqi <8140671+li-zhiqi@user.noreply.gitee.com>
2021-04-18 14:40:12 +0800

    14:40

--- main.cpp
+++ main.cpp
@@ -1,24 +1,78 @@
 #include "Factory.cpp"
+#include <iostream>
 
-void getInput(std::fstream &_Input, std::string &_Method, int &_Order, double &_dt, std::string &_Initial, int &_N, bool &_err_analysis, bool &_grid_refine_analysis){
-  _Input >> _Method >> _Order >> _dt
-	 >> _Initial  >> _N
-	 >> _err_analysis >> _grid_refine_analysis;
+#define tol 1e-8
+
+void getInput(std::fstream &_Input, int &_Index, std::string &_Method,
+	      int &_Order, double &_dt, std::string &_Initial, int &_N,
+	      int &_err_analysis, int &_grid_refine_analysis){
+  _Input >> _Index >> _Method >> _Order
+	 >> _dt >> _Initial  >> _N
+	 >>  _err_analysis >>  _grid_refine_analysis;
 }
 
 
 int main(int argc, char* argv[]){
   std::string garbage,Method,Initial;
-  double dt,T;
-  Vector4d u0;
-  int Order,N;
-  bool err_analysis,grid_refine_analysis;
+  double dt,mu,T,time,result;
+  Vector4d u0,v;
+  int Index,Order,N;
+  int err_analysis,grid_refine_analysis;
   std::fstream Input("Input");
-  getline(Input,garbage);
   while (getline(Input,garbage,'\n')){
-    getInput(Input,Method,Order,dt,Initial,N,err_analysis,grid_refine_analysis);
+    getInput(Input,Index,Method,Order,dt,Initial,N,err_analysis,grid_refine_analysis);
+    TimeIntegrator *pIntegrator = TimeIntegratorFactory::Instance()->
+      CreateIntegrator(std::make_pair(Method,Order));
+    u0 = initial_load(Initial,mu,T);
+    if ( Initial == "Initial1"){
+      if ( grid_refine_analysis ){
+	result = pIntegrator->Grid_Refine1(u0,dt,mu,T);
+	std::cout << "Problem " << Index << ": "
+	  //
+		  << "Analysis can be saw by runing .m file" << std::endl; 
+      }
+      else{
+	if ( err_analysis ){
+	  result = pIntegrator->err_Initial(time,u0,dt,mu,T);
+	  std::cout << "Problem " << Index << ": "
+		    << "err: " << result << ",CPU time: " << time << "(ms)" << std::endl
+		    << "orbit can be saw by runing .m file" << std::endl;
+	}
+	else{
+	  v = pIntegrator->n_steps(time,u0,dt,mu,N);
+	  std::cout << "Problem " << Index << ": "
+		    << "relust: (" << v(0) << "," << v(1) << ",0)"
+		    <<",CPU time: " << time << "(ms)" << std::endl
+		    << "orbit can be saw by runing .m file" << std::endl;
+	}
+      }
+    }
+    else if ( Initial == "Initial2"){
+      if ( grid_refine_analysis ){
+	result = pIntegrator->Grid_Refine2(tol,u0,dt,mu,N);
+	std::cout << "Problem " << Index << ": "
+	  //
+		  << "Analysis can be saw by runing .m file" << std::endl; 
+      }
+      else{
+	if ( err_analysis ){
+	  result = pIntegrator->err_Richardson(tol,time,u0,dt,mu,N);
+	  std::cout << "Problem " << Index << ": "
+		    << "err: " << result << ",CPU time: " << time << "(ms)" << std::endl
+		    << "orbit can be saw by runing .m file" << std::endl;
+	}
+	else{
+	  v = pIntegrator->n_steps(time,u0,dt,mu,N);
+	  std::cout << "Problem " << Index << ": "
+		    << "relust: (" << v(0) << "," << v(1) << ",0)"
+		    <<",CPU time: " << time << "(ms)" << std::endl
+		    << "orbit can be saw by runing .m file" << std::endl;
+	}
+      }
+    }
   }
-  std::cout << Method << std::endl << Order << std::endl << dt <<std::endl << Initial <<std::endl << N <<std::endl << err_analysis <<std::endl << grid_refine_analysis << std::endl;
+  Input.close();
+  std::cout << "All done!" << std::endl;
 
   return 0;
 }
