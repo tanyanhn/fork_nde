8b338450516358214aa24d2cbd4d8823050417b7
li_zhiqi <8140671+li-zhiqi@user.noreply.gitee.com>
2021-05-14 17:13:14 +0800

    1 17:13

--- project2/Multigrid.h
+++ project2/Multigrid.h
@@ -1,15 +1,134 @@
 #ifndef __MGGS_MULTIGRID__
 #define __MGGS_MULTIGRID__
 #include <iostream>
+#include <cmath>
+#include <map>
+
+class function{
+ public:
+  double f(double _x){
+     double tmp1 = sin(_x);
+     double tmp2 = cos(_x);
+     double tmp3 = exp(tmp1);
+     return -tmp1*tmp3+tmp2*tmp2*tmp3;
+  }
+};
+
+class full_weighting{
+public:
+  static double* action(double* _vec, int& n){
+    if ( n < 8 || n%2 != 0){
+      std::cerr << "No matching n!" << std::endl;
+      return NULL;
+    }
+    else{
+      double* vec = new double[n/2 - 1];
+      for (int i = 0; i < n/2 - 1 ; i++)
+	vec[i] = 0.25*(_vec[2*i]+2*_vec[2*i+1]+_vec[2*i+2]);
+      n = n/2;
+      return vec;
+    }  
+  }
+};
+
+class linear{
+public:
+  static double* action(double* _vec, int& n){
+    double* vec = new double[2*n - 1];
+    vec[0] = 0.5*_vec[0];
+    vec[2*n-2] = 0.5*_vec[n-2];
+    for (int i = 1; i < 2*n -2 ; i++){
+      if (i%2 == 1)
+	vec[i] = _vec[(i-1)/2];
+      else
+	vec[i] = 0.5*(_vec[i/2 - 1]+_vec[i/2]);
+    }
+    n = n*2;
+    return vec;
+  }
+};
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+class Multigrid: public RestrictionPolicy, public InterpolationPolicy{
+private:
+  function* u;
+  std::pair<double,double> boundary;
+  double* initial;
+  std::pair<int,double> criteria;
+public:
+  Multigrid();
+  Multigrid(std::pair<double,double> _boundary, double* _initial, std::pair<int,double> _criteria);
+  ~Multigrid();
+  void load_boundary(std::pair<double,double> _boundary);
+  std::pair<double,double> get_boundary() const;
+  void load_initial(double* _initial);
+  double* get_initial() const;
+  void load_criteria(std::pair<int,double> _criteria);
+  std::pair<int,double> get_criteria() const;
+  void test_restriction(int &n);
+  void test_interpolation(int &n);
+};
 
 
+template <class RestrictionPolicy, class InterpolationPolicy>
+Multigrid<RestrictionPolicy,InterpolationPolicy>::Multigrid(){
+  u = new function;
+  initial = NULL;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+Multigrid<RestrictionPolicy,InterpolationPolicy>::Multigrid(std::pair<double,double> _boundary, double* _initial, std::pair<int,double> _criteria){
+  u = new function;
+  boundary = _boundary;
+  initial = _initial;
+  criteria = _criteria;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+Multigrid<RestrictionPolicy,InterpolationPolicy>::~Multigrid(){
+  delete u;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+void Multigrid<RestrictionPolicy,InterpolationPolicy>::load_boundary(std::pair<double,double> _boundary){
+  boundary = _boundary;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+std::pair<double,double> Multigrid<RestrictionPolicy,InterpolationPolicy>::get_boundary() const{
+  return boundary;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+void Multigrid<RestrictionPolicy,InterpolationPolicy>::load_initial(double* _initial){
+  initial = _initial;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+double* Multigrid<RestrictionPolicy,InterpolationPolicy>::get_initial() const{
+  return initial;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+void Multigrid<RestrictionPolicy,InterpolationPolicy>::load_criteria(std::pair<int,double> _criteria){
+  criteria = _criteria;
+}
+
+template <class RestrictionPolicy, class InterpolationPolicy>
+std::pair<int,double> Multigrid<RestrictionPolicy,InterpolationPolicy>::get_criteria() const{
+  return criteria;
+}
 
 
+template <class RestrictionPolicy, class InterpolationPolicy>
+void Multigrid<RestrictionPolicy,InterpolationPolicy>::test_restriction(int &n){
+  initial = RestrictionPolicy().action(initial,n);
+}
 
-
-
-
-
+template <class RestrictionPolicy, class InterpolationPolicy>
+void Multigrid<RestrictionPolicy,InterpolationPolicy>::test_interpolation(int &n){
+  initial = InterpolationPolicy().action(initial,n);
+}
 
 
 
---
+++ project2/test.cpp
@@ -0,0 +1,17 @@
+#include "Multigrid.h"
+
+int main(int argc, char* argv[]){
+  std::pair<double,double> boud(-2.3,0.3);
+  std::pair<int,double> crit(0,1e-8);
+  int n = 8;
+  double* init = new double[n-1];
+  init[0]=1;init[1]=3;init[2]=6;
+  init[3]=1.4;init[4]=0;init[5]=-2;init[6]=2.4;
+  Multigrid<full_weighting,linear> V(boud,init,crit);
+  V.test_restriction(n);
+  V.test_interpolation(n);
+  double* p = V.get_initial();
+  for (int i = 0 ; i < n - 1 ; i++)
+    std::cout << p[i] << std::endl;
+  return 0;
+}
