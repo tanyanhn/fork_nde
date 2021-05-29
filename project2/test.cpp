1061b603cd7daeb9edd2eb9ae3af342faa0aebfb
li_zhiqi <8140671+li-zhiqi@user.noreply.gitee.com>
2021-05-29 16:59:38 +0800

    10 16:59

--- project2/test.cpp
+++ project2/test.cpp
@@ -3,13 +3,13 @@
 int main(int argc, char* argv[]){
   std::pair<double,double> boud(1,exp(sin(1)));
   std::pair<int,double> crit(1,1e-8);
-  int n = 256;
+  int n = 1024;
   double* init = new double[n-1];
   for (int i = 0 ; i < n-1 ; i++)
     init[i] = 0;
   Multigrid<full_weighting,linear,function1> V(boud,init,crit);
   double* f = V.righthand(n);
-  double result = V.analysis_V_cycle(init,n,f,200,200);
+  double result = V.two_norm(f,n);
   std::cout << "result:" << result << std::endl;
   /*
   std::cout << "result:" << std::endl;
